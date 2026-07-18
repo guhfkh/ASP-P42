70867c43a8e8c38a88ea4837953843d77291d003
8KOHAN <kohanzom@gmail.com>
2026-07-18 16:57:09 +0300

    Добавьте файлы проекта.

---
+++ ASP-P42/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using ASP_P42.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_P42.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Intro()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

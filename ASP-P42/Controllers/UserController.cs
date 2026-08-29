896a84836fad2b03e7d89b8390cfbc2f6fc43057
8KOHAN <kohanzom@gmail.com>
2026-08-29 18:06:42 +0300

    update

--- ASP-P42/Controllers/UserController.cs
+++ ASP-P42/Controllers/UserController.cs
@@ -1,30 +1,116 @@
 ﻿using ASP_P42.Data;
 using ASP_P42.Data.Entities;
 using ASP_P42.Services.Kdf;
+using ASP_P42.Services.Time;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Text;
+using System.Text.Json;
 
 namespace ASP_P42.Controllers
 {
     public class UserController(
             DataContext dataContext,
-            IKdfService kdfService
+            IKdfService kdfService,
+            ITimeService timeService
         ) : Controller
     {
         private readonly DataContext _dataContext = dataContext;
         private readonly IKdfService _kdfService = kdfService;
+        private readonly ITimeService _timeService = timeService;
         public IActionResult BasicAuth()
         {
-            string authHeader = HttpContext.Request.Headers.Authorization.ToString();
-            if(authHeader == string.Empty)
+            UserAccess? userAccess;
+            try
             {
-                return Unauthorized("Missing Autorization header");
+                userAccess = AuthenticateUser();
+            }
+            catch ( Exception ex )
+            {
+                return BadRequest(ex.Message);
+            }
+
+            if ( userAccess == null )
+            {
+                return Unauthorized(
+                        "Credentials rejected: chech login and password");
+            }
+
+            HttpContext.Session.SetString(
+                "userAccessId",
+                userAccess.Id.ToString()
+                );
+
+            return Ok();
+        }
+    
+        public IActionResult BasicAuthJwt()
+        {
+            UserAccess? userAccess;
+            try
+            {
+                userAccess = AuthenticateUser();
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+
+            if (userAccess == null)
+            {
+                return Unauthorized(
+                        "Credentials rejected: chech login and password");
+            }
+
+            var header = new
+            {
+                alg = "HS256",
+                typ = "JWT",
+            };
+
+            long time = _timeService.GetTimestamp();
+            var payload = new
+            {
+                sub = userAccess.Login,
+                iat = time,
+                exp = time + 1_000_000,
+                name = userAccess.UserData.FullName,
+                email = userAccess.UserData.Email,
+            };
+
+            string body = Base64UrlTextEncoder.Encode(
+                    Encoding.UTF8.GetBytes(
+                        JsonSerializer.Serialize(header)
+                    ))
+                    + "." +
+                    Base64UrlTextEncoder.Encode(
+                    Encoding.UTF8.GetBytes(
+                        JsonSerializer.Serialize(payload)
+                    )
+                );
+
+            string signature = Base64UrlTextEncoder.Encode(
+                    System.Security.Cryptography.HMACSHA256.HashData(
+                        Encoding.UTF8.GetBytes("secret"),
+                        Encoding.UTF8.GetBytes(body)
+                    ));
+
+            return Ok(body + "." + signature);
+        }
+
+        private UserAccess? AuthenticateUser()
+        {
+            string authHeader = HttpContext.Request.Headers.Authorization.ToString();
+            if (authHeader == string.Empty)
+            {
+                throw new Exception("Missing Autorization header");
             }
 
             string scheme = "Basic ";
             if (!authHeader.StartsWith(scheme))
             {
-                return Unauthorized(
+                throw new Exception(
                     "Authorization scheme must be 'Basic '");
             }
 
@@ -37,7 +123,7 @@ namespace ASP_P42.Controllers
             }
             catch (Exception)
             {
-                return Unauthorized(
+                throw new Exception(
                     "Authorization credentials must be valid Base64::section 4");
             }
 
@@ -48,7 +134,7 @@ namespace ASP_P42.Controllers
             }
             catch (Exception)
             {
-                return Unauthorized(
+                throw new Exception(
                     "User-pass must be valid UTF8 string");
             }
 
@@ -56,7 +142,7 @@ namespace ASP_P42.Controllers
 
             if (parts.Length != 2)
             {
-                return Unauthorized(
+                throw new Exception(
                     "User-pass must be concatenated by ':'");
             }
             string login = parts[0];
@@ -64,23 +150,19 @@ namespace ASP_P42.Controllers
 
             if (_dataContext
                 .UserAccesses
+                .Include(ua => ua.UserData)
+                .Include(ua => ua.UserRole)
+                .AsNoTracking()
                 .FirstOrDefault(ua => ua.Login == login)
                 is UserAccess userAccess)
             {
                 string dk = _kdfService.Dk(password, userAccess.Salt);
                 if (dk == userAccess.Dk)
                 {
-                    HttpContext.Session.SetString(
-                        "userAccessId",
-                        userAccess.Id.ToString()
-                        );
-
-                    return Ok();
+                    return userAccess;
                 }
             }
-
-            return Unauthorized(
-                    "Credentials rejected: chech login and password");
+            return null;
         }
     }
 }
--- ASP-P42/Middleware/AuthSession/AuthSessionMiddleware.cs
+++ ASP-P42/Middleware/AuthSession/AuthSessionMiddleware.cs
@@ -1,11 +1,56 @@
-﻿namespace ASP_P42.Middlewere.AuthSession
+﻿using ASP_P42.Data;
+using ASP_P42.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace ASP_P42.Middlewere.AuthSession
 {
     public class AuthSessionMiddleware(RequestDelegate next)
     {
         private readonly RequestDelegate _next = next;
 
-        public async Task InvokeAsync(HttpContext context)
+        public async Task InvokeAsync(
+            HttpContext context,
+            DataContext dataContext    
+        )
         {
+            string authKey = "userAccessId";
+            if (context.Request.Query.ContainsKey("logout"))
+            {
+                context.Session.Remove(authKey);
+                context.Response.Redirect(context.Request.Path);
+                return;
+            }
+
+            context.Items.Add("ItemKey", "Item Value");
+
+            if (context.Session.Keys.Contains(authKey))
+            {
+                string userAccessId = context.Session.GetString(authKey)!;
+
+                UserAccess? userAccess = dataContext
+                    .UserAccesses
+                    .Include(ua => ua.UserRole)
+                    .Include(ua => ua.UserData)
+                    .AsNoTracking()
+                    .FirstOrDefault(ua => ua.Id.ToString() == userAccessId);
+
+                if (userAccess != null)
+                {
+                    context.User = new ClaimsPrincipal(
+                        new ClaimsIdentity(
+                        [
+                            new Claim(ClaimTypes.Name, userAccess.UserData.FullName),
+                            new(ClaimTypes.Email, userAccess.UserData.Email),
+                            new(ClaimTypes.NameIdentifier, userAccess.Login),
+                            new(ClaimTypes.Sid, userAccess.Id.ToString()),
+                        ],
+                        nameof(AuthSessionMiddleware)
+                            )
+                        );
+                }
+            }
+
             await _next(context);
         }
     }

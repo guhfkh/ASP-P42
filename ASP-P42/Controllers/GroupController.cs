6b111ddc4796db95e7faf24f689125ca52cfb28c
8KOHAN <kohanzom@gmail.com>
2026-09-09 16:19:45 +0300

    Homework - API контролери

---
+++ ASP-P42/Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using ASP_P42.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_P42.Controllers
+{
+    public class GroupController(DataContext dataContext) : Controller
+    {
+        private readonly DataContext _dataContext = dataContext;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var groups = _dataContext.ProductGroups
+                .Where(g => g.IsHidden == 0)
+                .ToList();
+
+            return View(groups);
+        }
+    }
+}
